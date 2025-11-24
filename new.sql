8188d346d08eaed02ad084af4ca38f9da3d0e683
Ladipo Ezekiel Ipadeola <rileydrizzy@hotmail.com>
2025-11-24 03:23:55 +0000

    updates

--- new.sql
+++ new.sql
@@ -1,4 +1,20 @@
-INSERT INTO instructor
-VALUES ('77765', 'Farida', 10000, 'Biology'),
-    ('88976', 'Chukwu', 75000, 'Comp. Sci.'),
-    (7776, 'Uche', 10000, 'Biology');
+CREATE TABLE test(Deptname VARCHAR(20) PRIMARY KEY, id INT);
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    Deptname VARCHAR(20),
+    FOREIGN KEY (Deptname) REFERENCES test
+);
+CREATE TABLE course (
+    course_code INT PRIMARY KEY,
+    course_name VARCHAR(60),
+    Deptname VARCHAR(20),
+    FOREIGN KEY (Deptname) REFERENCES test
+);
+CREATE TABLE enroll (
+    students_id INT,
+    course_code INT,
+    PRIMARY KEY (students_id, course_code),
+    FOREIGN KEY (students_id) REFERENCES students(id),
+    FOREIGN KEY (course_code) REFERENCES course(course_code)
+);
